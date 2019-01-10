3fd2ab606106d39e2293fe4e02a9682e82fdea06
mdscheuerell <mark.scheuerell@noaa.gov>
2019-01-10 11:34:57 -0800

    finished R script for live coding

--- Labs/Week 1/lab_01b_intro_ts_functions.R
+++ Labs/Week 1/lab_01b_intro_ts_functions.R
@@ -1,31 +1,12 @@
-## ----ts-load-datasets, eval=FALSE----------------------------------------
-load("CO2_data.RData")
-load("NHemiTemp_data.RData")
-load("hourly_phyto.RData")
+library(here)
+labdir <- here("Labs/Week 1")
 
-## ----ts-loadpackages, warning=FALSE, message=FALSE, results='hide'-------
-library(stats)
-library(MARSS)
-library(forecast)
-library(datasets)
+## library(datasets)
 
-## ----ts-CO2data, eval=FALSE----------------------------------------------
-## library(RCurl)
-## ## get CO2 data from Mauna Loa observatory
-## ww1 <- "ftp://aftp.cmdl.noaa.gov/products/"
-## ww2 <- "trends/co2/co2_mm_mlo.txt"
-## CO2fulltext <- getURL(paste0(ww1,ww2))
-## CO2 <- read.table(text=CO2fulltext)[,c(1,2,5)]
-## ## assign better column names
-## colnames(CO2) <- c("year","month","ppm")
-## save(CO2, CO2fulltext, file="CO2_data.RData")
-
-## ----ts-temp-data, eval=FALSE--------------------------------------------
-## library(RCurl)
-## ww1 <- "https://www.ncdc.noaa.gov/cag/time-series/"
-## ww2 <- "global/nhem/land_ocean/p12/12/1880-2014.csv"
-## Temp <- read.csv(text=getURL(paste0(ww1,ww2)), skip=4)
-## save(Temp, file="NHemiTemp_data.RData")
+## load datasets
+load(file.path(labdir,"CO2_data.RData"))
+load(file.path(labdir,"NHemiTemp_data.RData"))
+load(file.path(labdir,"hourly_phyto.RData"))
 
 ## CO2 time series
 co2 <- ts(data=CO2$ppm, frequency=12,
@@ -38,23 +19,27 @@ plot.ts(co2, ylab=expression(paste("CO"[2]," (ppm)")))
 ## temperature time serires
 temp.ts <- ts(data=Temp$Value, frequency=12, start=c(1880,1))
 
-## intersection (only overlapping times)
-datI <- ts.intersect(co2,temp.ts)
-## dimensions of common-time data
-dim(datI)
 
 ## union (all times)
 datU <- ts.union(co2,temp.ts)
 ## dimensions of all-time data
 dim(datU)
+## plot both ts
+plot(datU, main="", yax.flip=TRUE)
 
+## intersection (only overlapping times)
+datI <- ts.intersect(co2,temp.ts)
+## dimensions of common-time data
+dim(datI)
 ## plot both ts
 plot(datI, main="", yax.flip=TRUE)
 
 
+
+## DECOMPOSITION
+
 ## weights for moving avg
 fltr <- c(1/2,rep(1,times=11),1/2)/12
-
 
 ## estimate of trend
 co2.trend <- filter(co2, filter=fltr, method="convo", sides=2)
@@ -120,6 +105,10 @@ co2.D2D12 <- diff(co2.D2, lag=12)
 ## plot the newly differenced data
 plot(co2.D2D12, ylab=expression(paste(nabla,"(",nabla^2,"CO"[2],")")))
 
+
+
+
+## ACF & PACF
 
 ## correlogram of the CO2 data
 acf(co2, lag.max=36)
@@ -197,56 +186,40 @@ plot.pacf <- function(PACFobj) {
   abline(h=0)
 }
 
-
 ## PACF of the CO2 data
 co2.pacf <- pacf(co2, plot = FALSE)
 ## correlogram of the CO2 data
 plot.pacf(co2.pacf)
 
-## ----ts-LynxSunspotCCF, eval=TRUE, echo=TRUE-----------------------------
+
+
+## CCF
+
 ## get the matching years of sunspot data
 suns <- ts.intersect(lynx,sunspot.year)[,"sunspot.year"]
 ## get the matching lynx data
 lynx <- ts.intersect(lynx,sunspot.year)[,"lynx"]
 
-## ----ts-plotSunsLynxEcho, eval=FALSE, echo=TRUE--------------------------
-## ## plot time series
-## plot(cbind(suns,lynx), yax.flip=TRUE)
-
-## ----ts-plotSunsLynx, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=6, fig.cap='(ref:ts-plotSunsLynx)'----
-## set the margins & text size
-par(mar=c(4,4,1,1), oma=c(0,0,0,0), cex=1)
-## plot the ts
+## plot both ts
 plot(cbind(suns,lynx), main="", yax.flip=TRUE)
 
-## ----ts-plotCCFa, eval=FALSE, echo=TRUE----------------------------------
-## ## CCF of sunspots and lynx
-## ccf(suns, log(lynx), ylab="Cross-correlation")
+## NOTE: for ccf(x, y), lags are evaluated at x_{t+k} vs y_t
+##       so it's often easier to use cor(x = y, y = x)
 
-## ----ts-plotCCFb, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotCCFb)'----
-## set the margins & text size
-par(mar=c(4,4,1,1), oma=c(0,0,0,0), cex=1)
 ## CCF of sunspots and lynx
-ccf(suns, lynx, ylab="Cross-correlation")
+ccf(lynx, suns, ylab="Cross-correlation")
 
-## ----ts-DWNsim, echo=TRUE, eval=TRUE-------------------------------------
+
+
+## WHITE NOISE
+
 set.seed(123)
 ## random normal variates
 GWN <- rnorm(n=100, mean=5, sd=0.2)
 ## random Poisson variates
 PWN <- rpois(n=50, lambda=20)
 
-## ----ts-DWNsimPlotEcho, echo=TRUE, eval=FALSE----------------------------
-## ## set up plot region
-## par(mfrow=c(1,2))
-## ## plot normal variates with mean
-## plot.ts(GWN)
-## abline(h=5, col="blue", lty="dashed")
-## ## plot Poisson variates with mean
-## plot.ts(PWN)
-## abline(h=20, col="blue", lty="dashed")
 
-## ----ts-plotDWNsims, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotDWNsims)'----
 ## set the margins & text size
 par(mar=c(4,4,1,1), oma=c(0,0,0,0), cex=1, mfrow=c(1,2))
 ## plot normal variates with mean
@@ -256,15 +229,7 @@ abline(h=5, col="blue", lty="dashed")
 plot.ts(PWN)
 abline(h=20, col="blue", lty="dashed")
 
-## ----ts-DWNacfEcho, echo=TRUE, eval=FALSE--------------------------------
-## ## set up plot region
-## par(mfrow=c(1,2))
-## ## plot normal variates with mean
-## acf(GWN, main="", lag.max=20)
-## ## plot Poisson variates with mean
-## acf(PWN, main="", lag.max=20)
 
-## ----ts-plotACFdwn, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotACFdwn)'----
 ## set the margins & text size
 par(mar=c(4,4,1,1), oma=c(0,0,0,0), cex=1, mfrow=c(1,2))
 ## plot normal variates with mean
@@ -272,7 +237,10 @@ acf(GWN, main="", lag.max=20)
 ## plot Poisson variates with mean
 acf(PWN, main="", lag.max=20)
 
-## ----ts-RWsim, eval=TRUE, echo=TRUE--------------------------------------
+
+
+## RW MODELS
+
 ## set random number seed
 set.seed(123)
 ## length of time series
@@ -282,18 +250,9 @@ xx <- ww <- rnorm(n=TT, mean=0, sd=1)
 ## compute values 2 thru TT
 for(t in 2:TT) { xx[t] <- xx[t-1] + ww[t] }
 
-## ----ts-plotRWecho, eval=FALSE, echo=TRUE--------------------------------
-## ## setup plot area
-## par(mfrow=c(1,2))
-## ## plot line
-## plot.ts(xx, ylab=expression(italic(x[t])))
-## ## plot ACF
-## plot.acf(acf(xx, plot=FALSE))
-
-## ----ts-calcRWACF, eval=TRUE, echo=FALSE---------------------------------
+## ACF of RW
 xx.acf <- acf(xx, plot=FALSE)
 
-## ----ts-plotRW, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotRW)'----
 ## setup plot area
 par(mfrow=c(1,2), mar=c(4,4,1,1), oma=c(0,0,0,0), cex=1)
 ## plot line
@@ -301,247 +260,14 @@ plot.ts(xx, ylab=expression(italic(x[t])))
 ## plot ACF
 plot.acf(xx.acf)
 
-## ----ts-RWsimAlt, eval=TRUE, echo=TRUE-----------------------------------
+## another method
 ## simulate RW
 x2 <- cumsum(ww)
 
-## ----ts-plotRWsimEcho, eval=FALSE, echo=TRUE-----------------------------
-## ## setup plot area
-## par(mfrow=c(1,2))
-## ## plot 1st RW
-## plot.ts(xx, ylab=expression(italic(x[t])))
-## ## plot 2nd RW
-## plot.ts(x2, ylab=expression(italic(x[t])))
-
-## ----ts-plotRWalt, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotRWalt)'----
 ## setup plot area
 par(mfrow=c(1,2), mar=c(4,4,1,1), oma=c(0,0,0,0), cex=1)
 ## plot 1st RW
 plot.ts(xx, ylab=expression(italic(x[t])))
 ## plot 2nd RW
 plot.ts(x2, ylab=expression(italic(x[t])))
-
-## ----ts-simAR1, echo=TRUE, eval=TRUE-------------------------------------
-set.seed(456)
-## list description for AR(1) model with small coef
-AR.sm <- list(order=c(1,0,0), ar=0.1, sd=0.1)
-## list description for AR(1) model with large coef
-AR.lg <- list(order=c(1,0,0), ar=0.9, sd=0.1)
-## simulate AR(1)
-AR1.sm <- arima.sim(n=50, model=AR.sm)
-AR1.lg <- arima.sim(n=50, model=AR.lg)
-
-## ----ts-plotAR1sims, eval=FALSE, echo=TRUE-------------------------------
-## ## setup plot region
-## par(mfrow=c(1,2))
-## ## get y-limits for common plots
-## ylm <- c(min(AR1.sm,AR1.lg), max(AR1.sm,AR1.lg))
-## ## plot the ts
-## plot.ts(AR1.sm, ylim=ylm,
-##         ylab=expression(italic(x)[italic(t)]),
-##         main=expression(paste(phi," = 0.1")))
-## plot.ts(AR1.lg, ylim=ylm,
-##         ylab=expression(italic(x)[italic(t)]),
-##         main=expression(paste(phi," = 0.9")))
-
-## ----ts-getPlotLims, eval=TRUE, echo=FALSE-------------------------------
-## get y-limits for common plots
-ylm <- c(min(AR1.sm,AR1.lg), max(AR1.sm,AR1.lg))
-
-## ----ts-plotAR1contrast, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotAR1contrast)'----
-## set the margins & text size
-par(mfrow=c(1,2), mar=c(4,4,1.5,1), oma=c(0,0,0,0), cex=1)
-## plot the ts
-plot.ts(AR1.sm, ylim=ylm,
-        ylab=expression(italic(x)[italic(t)]),
-        main=expression(paste(phi," = 0.1")))
-plot.ts(AR1.lg, ylim=ylm,
-        ylab=expression(italic(x)[italic(t)]),
-        main=expression(paste(phi," = 0.9")))
-
-## ----ts-simAR1opps, echo=TRUE, eval=TRUE---------------------------------
-set.seed(123)
-## list description for AR(1) model with small coef
-AR.pos <- list(order=c(1,0,0), ar=0.5, sd=0.1)
-## list description for AR(1) model with large coef
-AR.neg <- list(order=c(1,0,0), ar=-0.5, sd=0.1)
-## simulate AR(1)
-AR1.pos <- arima.sim(n=50, model=AR.pos)
-AR1.neg <- arima.sim(n=50, model=AR.neg)
-
-## ----ts-plotAR1oppsEcho, eval=FALSE, echo=TRUE---------------------------
-## ## setup plot region
-## par(mfrow=c(1,2))
-## ## get y-limits for common plots
-## ylm <- c(min(AR1.pos,AR1.neg), max(AR1.pos,AR1.neg))
-## ## plot the ts
-## plot.ts(AR1.pos, ylim=ylm,
-##         ylab=expression(italic(x)[italic(t)]),
-##         main=expression(paste(phi[1]," = 0.5")))
-## plot.ts(AR1.neg,
-##         ylab=expression(italic(x)[italic(t)]),
-##         main=expression(paste(phi[1]," = -0.5")))
-
-## ----ts-getPlotLimsOpps, eval=TRUE, echo=FALSE---------------------------
-## get y-limits for common plots
-ylm <- c(min(AR1.pos,AR1.neg), max(AR1.pos,AR1.neg))
-
-## ----ts-plotAR1opps, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotAR1opps)'----
-## set the margins & text size
-par(mfrow=c(1,2), mar=c(4,4,1.5,1), oma=c(0,0,0,0), cex=1)
-## plot the ts
-plot.ts(AR1.pos, ylim=ylm,
-        ylab=expression(italic(x)[italic(t)]),
-        main=expression(paste(phi[1]," = 0.5")))
-plot.ts(AR1.neg, ylim=ylm,
-        ylab=expression(italic(x)[italic(t)]),
-        main=expression(paste(phi[1]," = -0.5")))
-
-## ----ts-ARpFail, eval=FALSE, echo=TRUE-----------------------------------
-## arima.sim(n=100, model=list(order(2,0,0), ar=c(0.5,0.5)))
-
-## ----ts-ARpSims, eval=TRUE, echo=TRUE------------------------------------
-set.seed(123)
-## the 4 AR coefficients
-ARp <- c(0.7, 0.2, -0.1, -0.3)
-## empty list for storing models
-AR.mods <- list()
-## loop over orders of p
-for(p in 1:4) {
-  ## assume SD=1, so not specified
-  AR.mods[[p]] <- arima.sim(n=10000, list(ar=ARp[1:p]))
-}
-
-## ----ts-plotARpCompsEcho, eval=FALSE, echo=TRUE--------------------------
-## ## set up plot region
-## par(mfrow=c(4,3))
-## ## loop over orders of p
-## for(p in 1:4) {
-##   plot.ts(AR.mods[[p]][1:50],
-##           ylab=paste("AR(",p,")",sep=""))
-##   acf(AR.mods[[p]], lag.max=12)
-##   pacf(AR.mods[[p]], lag.max=12, ylab="PACF")
-## }
-
-## ----ts-plotARpComps, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=8, fig.cap='(ref:ts-plotARpComps)'----
-## set the margins & text size
-par(mfrow=c(4,3), mar=c(4,4,0.5,0.5), oma=c(0,0,0,0), cex=1)
-## loop over orders of p
-for(p in 1:4) {
-  plot.ts(AR.mods[[p]][1:50],ylab=paste("AR(",p,")",sep=""))
-  acf(AR.mods[[p]], lag.max=12)
-  pacf(AR.mods[[p]], lag.max=12, ylab="PACF")
-}
-
-## ----ts-simMA1opps, echo=TRUE, eval=TRUE---------------------------------
-set.seed(123)
-## list description for MA(1) model with small coef
-MA.sm <- list(order=c(0,0,1), ma=0.2, sd=0.1)
-## list description for MA(1) model with large coef
-MA.lg <- list(order=c(0,0,1), ma=0.8, sd=0.1)
-## list description for MA(1) model with large coef
-MA.neg <- list(order=c(0,0,1), ma=-0.5, sd=0.1)
-## simulate MA(1)
-MA1.sm <- arima.sim(n=50, model=MA.sm)
-MA1.lg <- arima.sim(n=50, model=MA.lg)
-MA1.neg <- arima.sim(n=50, model=MA.neg)
-
-## ----ts-plotMA1oppsEcho, eval=FALSE, echo=TRUE---------------------------
-## ## setup plot region
-## par(mfrow=c(1,3))
-## ## plot the ts
-## plot.ts(MA1.sm,
-##         ylab=expression(italic(x)[italic(t)]),
-##         main=expression(paste(theta," = 0.2")))
-## plot.ts(MA1.lg,
-##         ylab=expression(italic(x)[italic(t)]),
-##         main=expression(paste(theta," = 0.8")))
-## plot.ts(MA1.neg,
-##         ylab=expression(italic(x)[italic(t)]),
-##         main=expression(paste(theta," = -0.5")))
-
-## ----ts-plotMA1opps, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=3, fig.cap='(ref:ts-plotMA1opps)'----
-## set the margins & text size
-par(mfrow=c(1,3), mar=c(4,4,1.5,0.5), oma=c(0,0,0,0), cex=1)
-## plot the ts
-plot.ts(MA1.sm,
-        ylab=expression(italic(x)[italic(t)]),
-        main=expression(paste(theta," = 0.2")))
-plot.ts(MA1.lg,
-        ylab=expression(italic(x)[italic(t)]),
-        main=expression(paste(theta," = 0.8")))
-plot.ts(MA1.neg,
-        ylab=expression(italic(x)[italic(t)]),
-        main=expression(paste(theta," = -0.5")))
-
-## ----ts-MAqSims, eval=TRUE, echo=TRUE------------------------------------
-set.seed(123)
-## the 4 MA coefficients
-MAq <- c(0.7, 0.2, -0.1, -0.3)
-## empty list for storing models
-MA.mods <- list()
-## loop over orders of q
-for(q in 1:4) {
-  ## assume SD=1, so not specified
-  MA.mods[[q]] <- arima.sim(n=1000, list(ma=MAq[1:q]))
-}
-
-## ----ts-plotMApCompsEcho, eval=FALSE, echo=TRUE--------------------------
-## ## set up plot region
-## par(mfrow=c(4,3))
-## ## loop over orders of q
-## for(q in 1:4) {
-##   plot.ts(MA.mods[[q]][1:50],
-##           ylab=paste("MA(",q,")",sep=""))
-##   acf(MA.mods[[q]], lag.max=12)
-##   pacf(MA.mods[[q]], lag.max=12, ylab="PACF")
-## }
-
-## ----ts-plotMApComps, eval=TRUE, echo=FALSE, fig=TRUE, fig.height=8, fig.cap='(ref:ts-plotMApComps)'----
-## set the margins & text size
-par(mfrow=c(4,3), mar=c(4,4,0.5,0.5), oma=c(0,0,0,0), cex=1)
-## loop over orders of q
-for(q in 1:4) {
-  plot.ts(MA.mods[[q]][1:50],ylab=paste("MA(",q,")",sep=""))
-  acf(MA.mods[[q]], lag.max=12)
-  pacf(MA.mods[[q]], lag.max=12, ylab="PACF")
-}
-
-## ----ts-ARMAest, eval=TRUE, echo=TRUE------------------------------------
-set.seed(123)
-## ARMA(2,2) description for arim.sim()
-ARMA22 <- list(order=c(2,0,2), ar=c(-0.7,0.2), ma=c(0.7,0.2))
-## mean of process
-mu <- 5
-## simulated process (+ mean)
-ARMA.sim <- arima.sim(n=10000, model=ARMA22) + mu
-## estimate parameters
-arima(x=ARMA.sim, order=c(2,0,2))
-
-## ----ts-ARMAsearch1, eval=TRUE, echo=TRUE--------------------------------
-## empty list to store model fits
-ARMA.res <- list()
-## set counter
-cc <- 1
-## loop over AR
-for(p in 0:3) {
-  ## loop over MA
-  for(q in 0:3) {
-    ARMA.res[[cc]] <- arima(x=ARMA.sim,order=c(p,0,q))
-    cc <- cc + 1
-  }
-}
-## get AIC values for model evaluation
-ARMA.AIC <- sapply(ARMA.res,function(x) x$aic)
-## model with lowest AIC is the best
-ARMA.res[[which(ARMA.AIC==min(ARMA.AIC))]]
-
-## ----ts-autoARIMA, eval=TRUE, echo=TRUE----------------------------------
-## find best ARMA(p,q) model
-auto.arima(ARMA.sim, start.p=0, max.p=3, start.q=0, max.q=3)
-
-## ----ts-HW1_1, eval=FALSE, echo=TRUE-------------------------------------
-## ## what day of 2014 is Dec 1st?
-## dBegin <- as.Date("2014-12-01")
-## dayOfYear <- (dBegin - as.Date("2014-01-01") + 1)
 
