31f51e6f1e74443f57ac4e10e145a874da1643b4
EEH <eli.holmes@noaa.gov>
2021-01-21 16:17:54 -0800

    update with comments on code

---
+++ Lectures/Week 3/marss_example_0_with_comments.R
@@ -0,0 +1,129 @@
+# https://nwfsc-timeseries.github.io/atsa-labs/chap-mss.html
+
+# Set up
+library(MARSS)
+library(colorspace)
+hcl_palette_52 <- colorspace::sequential_hcl(52)
+hcl_palette_100 <- colorspace::sequential_hcl(100)
+library(ggplot2)
+        
+# Set up the data
+
+data(harborSealWA, package = "MARSS")
+dat <- MARSS::harborSealWA
+years <- dat[, "Year"]
+dat <- dat[, !(colnames(dat) %in% c("Year", "HC"))]
+dat <- t(dat)  # transpose to have years across columns
+colnames(dat) <- years
+n <- nrow(dat) - 1
+
+# Question one
+# Plot the data
+
+# Task Group 1
+# https://nwfsc-timeseries.github.io/atsa-labs/sec-mss-a-single-well-mixed-population.html
+# Fit a model with one underlying state (population process)
+# All sites having different observation variance
+mod.list.0 <- list(B = matrix(1), U = matrix("u"), Q = matrix("q"), 
+                   Z = matrix(1, 4, 1), A = "scaling", R = "diagonal and unequal", 
+                   x0 = matrix("mu"), tinitx = 0)
+fit.0 <- MARSS(dat, model = mod.list.0)
+# Look at the plots
+# Plot 1. Model fitted Y + CI
+# Plot 2. States - That's the population estimate
+# Last plot. Model innovations residuals ACF
+autoplot(fit.0)
+
+# Q1. Change the observation model to all sites
+# having the same observation error variance
+# Is this more or less supported than all having different variance?
+
+mod.list.1 <- list(B = matrix(1), U = matrix("u"), Q = matrix("q"), 
+                   Z = matrix(1, 4, 1), A = "scaling", R = "diagonal and equal", 
+                   x0 = matrix("mu"), tinitx = 0)
+fit.1 <- MARSS(dat, model = mod.list.1)
+
+# more supported
+c(AIC(fit.0), AIC(fit.1))
+
+# Q2. Change to observation errors R="unconstrained"
+# What does that mean? Are the errors correlated across sites? Why might that happen?
+
+mod.list.2 <- list(B = matrix(1), U = matrix("u"), Q = matrix("q"), 
+                   Z = matrix(1, 4, 1), A = "scaling", R = "unconstrained", 
+                   x0 = matrix("mu"), tinitx = 0)
+fit.2 <- MARSS(dat, model = mod.list.2)
+
+# less supported
+c(AIC(fit.0), AIC(fit.1), AIC(fit.2))
+
+# Means that the observation variances are correlated across sites
+
+# Q3. Look at the observation variance and correlation matrix
+M=coef(fit.2, type="matrix")$R # Variance matrix
+cov2cor(M) # Correlation matrix
+
+# What does this say about the observation errors?
+
+# Some of the errors seem to be correlated across sites, some positive, some negative
+
+
+
+# Task Group 2
+# https://nwfsc-timeseries.github.io/atsa-labs/sec-mss-segind.html
+# Fit a model with four underlying state (population process)
+mod.list.1 <- list(B = diag(1,4), U = matrix("u",4,1), Q = "diagonal and equal", 
+                   Z = diag(1,4), A = "scaling", R = "diagonal and unequal", 
+                   x0 = "unequal", tinitx = 0)
+fit.1 <- MARSS(dat, model = mod.list.1)
+# Q1. Look at the plots
+autoplot(fit.1)
+
+# Plot 2. States - That's the population estimate
+# Q2. In the model you fit, are the 4 state processes (the lines) correlated with each other? Just based on the model you fit? Think about Q.
+
+# No, not correlated. A good year in subpop 1 does not imply a good year in subpop 2.
+
+# Q3. What does this model say about the observation variances? Same, different?
+
+# Different levels of variance and independent errors.
+
+# Q4. Are we assuming that good years are correlated across sites or uncorrelated?
+
+# uncorrelated.
+
+# Task Group 3
+# https://nwfsc-timeseries.github.io/atsa-labs/sec-mss-segind.html
+# Fit a model with four underlying state (population process)
+mod.list.2 <- list(B = diag(1,4), U = matrix("u",4,1), Q = "equalvarcov", 
+                   Z = diag(1,4), A = "scaling", R = "diagonal and equal", 
+                   x0 = "unequal", tinitx = 0)
+fit.2 <- MARSS(dat, model = mod.list.2)
+# Q1. Look at the plots
+autoplot(fit.2)
+
+# Plot 2. States - That's the population estimate
+# Q2. In the model you specified, is the variability in the 4 state processes (the lines) correlated with each other? Think about Q.
+
+# Correlated but with the same variance. A good year in subpop 1 does not imply a good year in subpop 2.
+
+Q.diag      0.01218
+Q.offdiag   0.00983
+
+So pretty highly correlated.
+
+
+# Q3. Compare to a model with Q="diagonal and equal" and R="equalvarcov". Which one fits the data better based on AICc?
+
+mod.list.3 <- list(B = diag(1,4), U = matrix("u",4,1), Q = "diagonal and equal", 
+                   Z = diag(1,4), A = "scaling", R = "equalvarcov", 
+                   x0 = "unequal", tinitx = 0)
+fit.3 <- MARSS(dat, model = mod.list.3)
+
+# The model with R="equalvarcov" is less supported
+c(AIC(fit.2), AIC(fit.3))
+
+# Q4. How are these 2 models different? How are they different in terms of how they
+# model correlation across the sites?
+
+# The first says the subpopulations are correlated (good & bad years correlated across subpopulations). The second one says that subpopulations are uncorrelated but the observation errors are correlated.
--- Lectures/Week 3/univariate_example_0.R
+++ Lectures/Week 3/univariate_example_0.R
@@ -30,15 +30,51 @@ autoplot(forecast(fit, h=10))
 # Change the x (state) model to x_t = x_{t-1} + u
 # Don't change the y (observation) model
 # What happens to the fit and forecast? Why?
+mod.list <- list(
+  U = matrix("u"),
+  x0 = matrix("x0"),
+  B = matrix(1),
+  Q = matrix(0),
+  Z = matrix(1),
+  A = matrix(0),
+  R = matrix("r"),
+  tinitx = 0
+)
+fit <- MARSS(Nile, model=mod.list)
+autoplot(forecast(fit, h=10))
+
 
 # Question two
 # Change the x (state) model to x_t = x_{t-1} + w_t
 # Don't change the y (observation) model
 # What happens to the fit and forecast? Why?
+mod.list <- list(
+  U = matrix(0),
+  x0 = matrix("x0"),
+  B = matrix(1),
+  Q = matrix("q"),
+  Z = matrix(1),
+  A = matrix(0),
+  R = matrix("r"),
+  tinitx = 0
+)
+fit <- MARSS(Nile, model=mod.list)
+autoplot(forecast(fit, h=10))
 
 # Question three
-# Change the x (state) model to x_t = u
+# Change the x (state) model to x_t = u + w_t
 # Don't change the y (observation) model
 # Also SET x0 to 0 for this question; x0 = matrix(0). Otherwise the problem is underconstrained.
 # What happens to the fit and forecast? Why?
-
+mod.list <- list(
+  U = matrix("u"),
+  x0 = matrix(0),
+  B = matrix(0),
+  Q = matrix("q"),
+  Z = matrix(1),
+  A = matrix(0),
+  R = matrix("r"),
+  tinitx = 0
+)
+fit <- MARSS(Nile, model=mod.list)
+autoplot(forecast(fit, h=10))
