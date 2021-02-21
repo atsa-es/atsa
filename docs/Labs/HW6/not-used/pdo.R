775ae8300ff7c07e76e4505649272c5748beefd8
Mark Scheuerell <scheuerl@uw.edu>
2021-02-21 11:11:32 -0800

    rebuilt site

--- docs/Labs/HW6/not-used/pdo.R
+++ docs/Labs/HW6/not-used/pdo.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+# Something wrong with this
 url="https://www.ncdc.noaa.gov/teleconnections/pdo/data.csv"
 download.file(url, "pdo.csv")
 pdo_dat <- read_csv("pdo.csv", skip=1)
@@ -9,14 +10,15 @@ pdo_mon <- pdo_dat %>%
   select(date, pdo) %>%
   mutate(year = as.integer(sub("(^[0-9]{4})([0-9]{2})", "\\1", date)),
          month = as.integer(sub("(^[0-9]{4})([0-9]{2})", "\\2", date)),
-         ## assign summer & winter months to `period`
-         period = ifelse(month >= 4 & month <= 9, "summer", "winter"),
+         period = ifelse(month >= 3 & month <= 9, "summer", "winter"),
          date = NULL) %>%
   select(year, month, period, pdo)
+pdo_mon$year[pdo_mon$month %in% 10:12] <- pdo_mon$year[pdo_mon$month %in% 10:12] + 1
 
 ## create period means by year
 pdo_yr <- pdo_mon %>%
-  mutate(brood_year = year + 2) %>%
-  group_by(brood_year, period) %>%
+  group_by(year, period) %>%
   summarise(pdo = mean(pdo)) %>%
   pivot_wider(names_from = period, values_from = pdo)
+
+save("pdo_yr", file="pdo.RData")
